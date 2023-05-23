849e9d0c850be11aa0c5dd8c7fb8971a419d79fd
VarunPitty21 <73223338+VarunPitty21@users.noreply.github.com>
2023-05-23 09:12:10 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,1 @@
-module.exports = {
-    aws: {
-        access_key: "AKIAZPJDB2SA5PJREJZO",
-        secret_key: "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",
-        region: 'ap-south-1',
-        bucket_name: 'interactbe'
-    },
-    profile: 'user_profile.jpg',
-};
+
