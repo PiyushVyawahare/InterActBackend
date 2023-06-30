ddfa938dd83ef845599a9a4be4181a00a5e72cdb
piyus <piyushvyawahare2001@gmail.com>
2023-06-30 19:24:31 +0530

    Authentication

--- db/schema/otp/index.js
+++ db/schema/otp/index.js
@@ -2,21 +2,21 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const otpSchema = new Schema({
-    user_id : {
-        type : String,
-        required : true
-    },
-    otp : {
-        type: String,
-        required: true
-    },
-    expiry: {
-        type: Number,
-        required: true,
-    }
+  user_id: {
+    type: String,
+    required: true
+  },
+  otp: {
+    type: String,
+    required: true
+  },
+  expiry: {
+    type: Date,
+    required: true,
+  }
 },
-{
-    timestamps : true
-});
+  {
+    timestamps: true
+  });
 
-module.exports = mongoose.model('otp',otpSchema);
+module.exports = mongoose.model('otp', otpSchema);
